2829b95c77912f3541a9e1a5a3600be7e47c309b
KrPrince19 <princekumarydv19@gmail.com>
2025-10-22 20:28:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://fragzone.vercel.app', // ✅ Allow only your frontend
+  origin: 'https://fragzone.vercel.app/', // ✅ Allow only your frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
